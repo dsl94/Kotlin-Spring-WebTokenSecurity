e3e4abcc87b9793af1c09175ee445ba4c9a66f51
Nemanja <nemanjabosnjane@gmail.com>
2017-06-12 22:26:31 +0200

    Small refactoring

--- src/main/kotlin/com/example/kotlin/jwt/kotlinjwt/security/dto/AccountCredentials.kt
+++ src/main/kotlin/com/example/kotlin/jwt/kotlinjwt/security/dto/AccountCredentials.kt
@@ -4,6 +4,6 @@ package com.example.kotlin.jwt.kotlinjwt.security.dto
  * Created by Nemanja on 6/12/17.
  */
 data class AccountCredentials(
-        var username: String = "",
-        var password: String = ""
+        var username: String?,
+        var password: String?
 )
